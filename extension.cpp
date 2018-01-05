ab5a5cbb7e03e73d619c6e47461a86744a10ce64
Rokhaug <dir1@nvlsi.no>
2018-01-05 11:13:06 +0100

    add simple functionality

---
+++ extension.cpp
@@ -0,0 +1,13 @@
+#include "pxt.h"
+using namespace pxt;
+namespace cpptest {
+    /**
+     * a simle function adding the two parameters
+     * @param a [0-3] the first parameter eg: 2
+     * @param b the other parameter eg: 3
+     */
+    //% blockId="extfun" block="%a| + %b" 
+    int extfun(int x, int y) {
+        return x + y;
+    }
+}
---
+++ shims.d.ts
@@ -0,0 +1,13 @@
+// Auto-generated. Do not edit.
+declare namespace cpptest {
+
+    /**
+     * a simle function adding the two parameters
+     * @param a [0-3] the first parameter eg: 2
+     * @param b the other parameter eg: 3
+     */
+    //% blockId="extfun" block="%a| + %b" shim=cpptest::extfun
+    function extfun(x: number, y: number): number;
+}
+
+// Auto-generated. Do not edit. Really.
